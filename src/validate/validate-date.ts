1c68a4e84ea24d9e9ffcf67732d1b2d11037c606
André Gaul <andre@gaul.io>
2018-10-20 19:10:38 +0200

    validate: fix date typo

--- src/validate/validate-date.ts
+++ src/validate/validate-date.ts
@@ -1,8 +1,8 @@
 import { FefeError } from '../errors'
 
 export function validateDate () {
-  return (value: unknown) => {
-    if (value instanceof Date) throw new FefeError(value, 'Not a date.')
+  return (value: unknown): Date => {
+    if (!(value instanceof Date)) throw new FefeError(value, 'Not a date.')
     return value
   }
 }
