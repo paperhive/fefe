c18e5ad786aa9f264e110370cffabaa8670a1370
André Gaul <andre@gaul.io>
2018-10-22 10:45:04 +0200

    validate: test with isNaN in validateNumber()

--- src/validate/validate-number.ts
+++ src/validate/validate-number.ts
@@ -5,7 +5,7 @@ export function validateNumber (
 ) {
   return (value: unknown) => {
     // tslint:disable-next-line:strict-type-predicates
-    if (typeof value !== 'number') throw new FefeError(value, 'Not a number.')
+    if (typeof value !== 'number' || Number.isNaN(value)) throw new FefeError(value, 'Not a number.')
     if (min !== undefined && value < min) throw new FefeError(value, `Less than ${min}.`)
     if (max !== undefined && value > max) throw new FefeError(value, `Greater than ${max}.`)
     return value
