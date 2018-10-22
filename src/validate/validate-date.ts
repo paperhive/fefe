8614485a2a4e58867adf12f8ebdc4f66e739e032
André Gaul <andre@gaul.io>
2018-10-22 09:41:31 +0200

    validate: check if valid date in validateDate()

--- src/validate/validate-date.ts
+++ src/validate/validate-date.ts
@@ -3,6 +3,7 @@ import { FefeError } from '../errors'
 export function validateDate () {
   return (value: unknown): Date => {
     if (!(value instanceof Date)) throw new FefeError(value, 'Not a date.')
+    if (isNaN(value.getTime())) throw new FefeError(value, 'Not a valid date.')
     return value
   }
 }
