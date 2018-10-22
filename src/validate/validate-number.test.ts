84a46e7c44ca5343179fa2a0ded7635c36974b4e
André Gaul <andre@gaul.io>
2018-10-22 10:51:28 +0200

    validate: add tests for validateNumber()

---
+++ src/validate/validate-number.test.ts
@@ -0,0 +1,33 @@
+import { expect } from 'chai'
+
+import { FefeError } from '../errors'
+import { validateNumber } from './validate-number'
+
+describe('validateNumber()', () => {
+  it('should throw if not a number', () => {
+    expect(() => validateNumber()('foo'))
+      .to.throw(FefeError, 'Not a number.')
+  })
+
+  it('should throw if NaN', () => {
+    expect(() => validateNumber()(1 / 0 - 1 / 0))
+      .to.throw(FefeError, 'NaN is not allowed.')
+  })
+
+  it('should throw if infinite', () => {
+    expect(() => validateNumber()(1 / 0))
+      .to.throw(FefeError, 'Infinity is not allowed.')
+  })
+
+  it('should throw if less than min', () => {
+    expect(() => validateNumber({ min: 0 })(-1))
+      .to.throw(FefeError, 'Less than 0.')
+  })
+
+  it('should throw if less than max', () => {
+    expect(() => validateNumber({ max: 0 })(11))
+      .to.throw(FefeError, 'Greater than 0.')
+  })
+
+  it('return a valid number', () => expect(validateNumber()(1)).to.equal(1))
+})
--- src/validate/validate-number.ts
+++ src/validate/validate-number.ts
@@ -1,11 +1,14 @@
 import { FefeError } from '../errors'
 
 export function validateNumber (
-  { min, max }: { min?: number, max?: number } = {}
+  { min, max, allowNaN = false, allowInfinity = false }:
+  { min?: number, max?: number, allowNaN?: boolean, allowInfinity?: boolean} = {}
 ) {
   return (value: unknown) => {
     // tslint:disable-next-line:strict-type-predicates
-    if (typeof value !== 'number' || Number.isNaN(value)) throw new FefeError(value, 'Not a number.')
+    if (typeof value !== 'number') throw new FefeError(value, 'Not a number.')
+    if (!allowNaN && Number.isNaN(value)) throw new FefeError(value, 'NaN is not allowed.')
+    if (!allowInfinity && !Number.isFinite(value)) throw new FefeError(value, 'Infinity is not allowed.')
     if (min !== undefined && value < min) throw new FefeError(value, `Less than ${min}.`)
     if (max !== undefined && value > max) throw new FefeError(value, `Greater than ${max}.`)
     return value
