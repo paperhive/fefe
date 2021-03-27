e539e78e7d2539bf36ff196f651e1d4cc15e121f
André Gaul <andre@gaul.io>
2021-03-27 18:52:04 +0100

    migrate number() to either

--- src/number.ts
+++ src/number.ts
@@ -1,4 +1,6 @@
-import { FefeError } from './errors'
+import { leafError } from './errors'
+import { failure, success } from './result'
+import { Validator2 } from './validate'
 
 export interface NumberOptions {
   min?: number
@@ -14,20 +16,20 @@ export function number({
   integer,
   allowNaN = false,
   allowInfinity = false,
-}: NumberOptions = {}) {
-  return (value: unknown): number => {
-    // tslint:disable-next-line:strict-type-predicates
-    if (typeof value !== 'number') throw new FefeError(value, 'Not a number.')
+}: NumberOptions = {}): Validator2<number> {
+  return (value: unknown) => {
+    if (typeof value !== 'number')
+      return failure(leafError(value, 'Not a number.'))
     if (!allowNaN && Number.isNaN(value))
-      throw new FefeError(value, 'NaN is not allowed.')
+      return failure(leafError(value, 'NaN is not allowed.'))
     if (!allowInfinity && !Number.isFinite(value))
-      throw new FefeError(value, 'Infinity is not allowed.')
+      return failure(leafError(value, 'Infinity is not allowed.'))
     if (integer && !Number.isInteger(value))
-      throw new FefeError(value, 'Not an integer.')
+      return failure(leafError(value, 'Not an integer.'))
     if (min !== undefined && value < min)
-      throw new FefeError(value, `Less than ${min}.`)
+      return failure(leafError(value, `Less than ${min}.`))
     if (max !== undefined && value > max)
-      throw new FefeError(value, `Greater than ${max}.`)
-    return value
+      return failure(leafError(value, `Greater than ${max}.`))
+    return success(value)
   }
 }
