50a15fa41a606d6083c10ab581477e307c97bd74
André Gaul <andre@gaul.io>
2021-03-27 18:36:46 +0100

    migrate date() to either

--- src/date.test.ts
+++ src/date.test.ts
@@ -1,33 +1,40 @@
-import { expect } from 'chai'
+import { assert } from 'chai'
 
-import { FefeError } from './errors'
+import { leafError } from './errors'
 import { date } from './date'
+import { failure, success } from './result'
 
 describe('date()', () => {
-  it('should throw if not a date', () => {
-    expect(() => date()('foo')).to.throw(FefeError, 'Not a date.')
-  })
-
-  it('should throw if not a valid date', () => {
-    expect(() => date()(new Date('foo'))).to.throw(
-      FefeError,
-      'Not a valid date.'
+  it('should return an error if not a date', () => {
+    assert.deepStrictEqual(
+      date()('foo'),
+      failure(leafError('foo', 'Not a date.'))
     )
   })
 
-  it('should throw if before min', () => {
+  it('should return an error if not a valid date', () => {
+    const value = new Date('foo')
+    assert.deepStrictEqual(
+      date()(value),
+      failure(leafError(value, 'Not a valid date.'))
+    )
+  })
+
+  it('should return an error if before min', () => {
     const validate = date({ min: new Date('2018-10-22T00:00:00.000Z') })
-    expect(() => validate(new Date('2018-10-21T00:00:00.000Z'))).to.throw(
-      FefeError,
-      'Before 2018-10-22T00:00:00.000Z.'
+    const value = new Date('2018-10-21T00:00:00.000Z')
+    assert.deepStrictEqual(
+      validate(value),
+      failure(leafError(value, 'Before 2018-10-22T00:00:00.000Z.'))
     )
   })
 
-  it('should throw if after max', () => {
+  it('should return an error if after max', () => {
     const validate = date({ max: new Date('2018-10-22T00:00:00.000Z') })
-    expect(() => validate(new Date('2018-10-23T00:00:00.000Z'))).to.throw(
-      FefeError,
-      'After 2018-10-22T00:00:00.000Z.'
+    const value = new Date('2018-10-23T00:00:00.000Z')
+    assert.deepStrictEqual(
+      validate(value),
+      failure(leafError(value, 'After 2018-10-22T00:00:00.000Z.'))
     )
   })
 
@@ -36,8 +43,7 @@ describe('date()', () => {
       min: new Date('2018-10-20T00:00:00.000Z'),
       max: new Date('2018-10-22T00:00:00.000Z'),
     })
-    const unsafeDate = new Date('2018-10-21T00:00:00.000Z')
-    const validatedDate: Date = validate(unsafeDate)
-    expect(validate(validatedDate)).to.equal(unsafeDate)
+    const value = new Date('2018-10-21T00:00:00.000Z')
+    assert.deepStrictEqual(validate(value), success(value))
   })
 })
--- src/date.ts
+++ src/date.ts
@@ -1,18 +1,22 @@
-import { FefeError } from './errors'
+import { leafError } from './errors'
+import { failure, success } from './result'
+import { Validator2 } from './validate'
 
 export interface DateOptions {
   min?: Date
   max?: Date
 }
 
-export function date({ min, max }: DateOptions = {}) {
-  return (value: unknown): Date => {
-    if (!(value instanceof Date)) throw new FefeError(value, 'Not a date.')
-    if (isNaN(value.getTime())) throw new FefeError(value, 'Not a valid date.')
+export function date({ min, max }: DateOptions = {}): Validator2<Date> {
+  return (value: unknown) => {
+    if (!(value instanceof Date))
+      return failure(leafError(value, 'Not a date.'))
+    if (isNaN(value.getTime()))
+      return failure(leafError(value, 'Not a valid date.'))
     if (min !== undefined && value.getTime() < min.getTime())
-      throw new FefeError(value, `Before ${min.toISOString()}.`)
+      return failure(leafError(value, `Before ${min.toISOString()}.`))
     if (max !== undefined && value.getTime() > max.getTime())
-      throw new FefeError(value, `After ${max.toISOString()}.`)
-    return value
+      return failure(leafError(value, `After ${max.toISOString()}.`))
+    return success(value)
   }
 }
