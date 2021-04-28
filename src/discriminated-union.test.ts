4e8d246d6664b13ece7cd707bc5cbdce34062b9d
André Gaul <andre@gaul.io>
2021-04-29 00:03:13 +0200

    update test

--- src/discriminated-union.test.ts
+++ src/discriminated-union.test.ts
@@ -7,21 +7,21 @@ import { number } from './number'
 import { optional } from './object'
 
 describe('discriminatedUnion()', () => {
-  const validateVehicle = discriminatedUnion('type', {
+  const validate = discriminatedUnion('type', {
     person: { age: number(), height: optional(number()) },
     robot: { battery: number() },
   })
 
   it('should return an error if not an object', () =>
     assert.deepStrictEqual(
-      validateVehicle(1),
+      validate(1),
       failure(leafError(1, 'Not an object.'))
     ))
 
   it('should return an error if type does not exist.', () => {
     const value = { type: 'cat', age: 12 }
     assert.deepStrictEqual(
-      validateVehicle(value),
+      validate(value),
       failure(
         branchError(value, [
           { key: 'type', error: leafError('cat', 'Not one of person, robot.') },
@@ -33,7 +33,7 @@ describe('discriminatedUnion()', () => {
   it('should return an error if key does not exist for type.', () => {
     const value = { type: 'robot', age: 12 }
     assert.deepStrictEqual(
-      validateVehicle(value),
+      validate(value),
       failure(leafError(value, 'Properties not allowed: age.'))
     )
   })
@@ -41,7 +41,7 @@ describe('discriminatedUnion()', () => {
   it('should return an error if key does not validate.', () => {
     const value = { type: 'person', age: 'foo' }
     assert.deepStrictEqual(
-      validateVehicle(value),
+      validate(value),
       failure(
         branchError(value, [
           { key: 'age', error: leafError('foo', 'Not a number.') },
@@ -52,15 +52,15 @@ describe('discriminatedUnion()', () => {
 
   it('should validate a person and a robot', () => {
     assert.deepStrictEqual(
-      validateVehicle({ type: 'person', age: 2 }),
+      validate({ type: 'person', age: 2 }),
       success({ type: 'person' as const, age: 2 })
     )
     assert.deepStrictEqual(
-      validateVehicle({ type: 'person', age: 2, height: 180 }),
+      validate({ type: 'person', age: 2, height: 180 }),
       success({ type: 'person' as const, age: 2, height: 180 })
     )
     assert.deepStrictEqual(
-      validateVehicle({ type: 'robot', battery: 99 }),
+      validate({ type: 'robot', battery: 99 }),
       success({ type: 'robot' as const, battery: 99 })
     )
   })
